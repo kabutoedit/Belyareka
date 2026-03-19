7463e84375147e39ce3cc5affc63c06f4f86da5e
kabutoedit <kolya2007niger@gmail.com>
2026-03-19 14:06:41 +0600

    url changes

--- belya-reka-front/src/api.ts
+++ belya-reka-front/src/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
 
-export const API_URL = import.meta.env.VITE_API_URL || "https://belayareka.anti-flow.com/";
+// export const API_URL = import.meta.env.VITE_API_URL || "https://belayareka.anti-flow.com/";
+export const API_URL = import.meta.env.VITE_API_URL || "https://belayareka.kg/";
 export const $api = axios.create({ baseURL: `${API_URL}/api` });
