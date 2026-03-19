9b36f69506ba2c57188fc31e784df72308653995
kabutoedit <kolya2007niger@gmail.com>
2026-03-19 18:26:07 +0600

    setting for strapi

--- br-backend/config/middlewares.ts
+++ br-backend/config/middlewares.ts
@@ -1,65 +1,68 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:', 'http:'],
-          'img-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'https://belaya-reka-storage.fra1.digitaloceanspaces.com',
-            '*.digitaloceanspaces.com',
-            'dl.airtable.com',
-          ],
-          'media-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'https://belaya-reka-storage.fra1.digitaloceanspaces.com',
-            '*.digitaloceanspaces.com',
-          ],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: [
-        'http://localhost:2000',
-        'http://localhost:3000',
-        'http://localhost:1337',
-        'http://68.183.209.243',
-        'http://68.183.209.243:1337',
-        'https://belayareka.ru',
-        'https://www.belayareka.ru',
-      ],
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'X-Requested-With'],
-      keepHeaderOnError: true,
-      credentials: true,
-    },
-  },
-  'strapi::poweredBy',
-  'strapi::query',
-  {
-    name: 'strapi::body',
-    config: {
-      formLimit: '256mb',
-      jsonLimit: '256mb',
-      textLimit: '256mb',
-      formidable: {
-        maxFileSize: 250 * 1024 * 1024,
-      },
-    },
-  },
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+	'strapi::logger',
+	'strapi::errors',
+	{
+		name: 'strapi::security',
+		config: {
+			contentSecurityPolicy: {
+				useDefaults: true,
+				directives: {
+					'connect-src': ["'self'", 'https:', 'http:'],
+					'img-src': [
+						"'self'",
+						'data:',
+						'blob:',
+						'https://belaya-reka-storage.fra1.digitaloceanspaces.com',
+						'*.digitaloceanspaces.com',
+						'*.strapi.io',
+						'dl.airtable.com',
+						'belayareka.kg',
+						'www.belayareka.kg',
+					],
+					'media-src': [
+						"'self'",
+						'data:',
+						'blob:',
+						'https://belaya-reka-storage.fra1.digitaloceanspaces.com',
+						'*.digitaloceanspaces.com',
+					],
+					upgradeInsecureRequests: null,
+				},
+			},
+		},
+	},
+	{
+		name: 'strapi::cors',
+		config: {
+			origin: [
+				'http://localhost:2000',
+				'http://localhost:3000',
+				'http://localhost:1337',
+				'https://belayareka.kg',
+				'https://www.belayareka.kg',
+				'https://belayareka.ru',
+				'https://www.belayareka.ru',
+			],
+			methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+			headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+			keepHeaderOnError: true,
+			credentials: true,
+		},
+	},
+	'strapi::poweredBy',
+	'strapi::query',
+	{
+		name: 'strapi::body',
+		config: {
+			formLimit: '256mb',
+			jsonLimit: '256mb',
+			textLimit: '256mb',
+			formidable: {
+				maxFileSize: 256 * 1024 * 1024, // 256mb
+			},
+		},
+	},
+	'strapi::session',
+	'strapi::favicon',
+	'strapi::public',
+]
--- br-backend/config/plugins.ts
+++ br-backend/config/plugins.ts
@@ -1,28 +1,36 @@
 export default ({ env }) => ({
-  upload: {
-    config: {
-      provider: env('UPLOAD_PROVIDER', 'local'),
-      providerOptions: env('UPLOAD_PROVIDER') === 'aws-s3' ? {
-        baseUrl: env('SPACES_CDN_URL'),
-        rootPath: env('SPACES_DIRECTORY'),
-        s3Options: {
-          credentials: {
-            accessKeyId: env('SPACES_ACCESS_KEY'),
-            secretAccessKey: env('SPACES_SECRET_KEY'),
-          },
-          endpoint: env('SPACES_ENDPOINT'),
-          region: env('SPACES_REGION'),
-          params: {
-            ACL: env('SPACES_ACL', 'public-read'),
-            Bucket: env('SPACES_BUCKET'),
-          },
-        },
-      } : {},
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
-      },
-    },
-  },
-});
+	upload: {
+		config: {
+			provider: env('UPLOAD_PROVIDER', 'local'),
+			providerOptions:
+				env('UPLOAD_PROVIDER') === 'aws-s3'
+					? {
+							s3Options: {
+								credentials: {
+									accessKeyId: env('SPACES_ACCESS_KEY'),
+									secretAccessKey: env('SPACES_SECRET_KEY'),
+								},
+								endpoint: env('SPACES_ENDPOINT'), // e.g. https://fra1.digitaloceanspaces.com
+								region: env('SPACES_REGION'),
+							},
+							params: {
+								ACL: env('SPACES_ACL', 'public-read'),
+								Bucket: env('SPACES_BUCKET'),
+							},
+							baseUrl: env('SPACES_CDN_URL'), // e.g. https://belaya-reka-storage.fra1.cdn.digitaloceanspaces.com
+							rootPath: env('SPACES_DIRECTORY'),
+						}
+					: {},
+			actionOptions: {
+				upload: {},
+				uploadStream: {},
+				delete: {},
+			},
+		},
+	},
+	'users-permissions': {
+		config: {
+			registerConfirmsEmail: false,
+		},
+	},
+})
